831445c32494b0d86c4e2437c0a09417ba1c418c
Gergely Imreh <imrehg@gmail.com>
2016-08-09 18:40:54 +0800

    incorporate changes from chat
    * Store host keys on `/data` and link to config from there
    * Able to load multiple client keys from env vars, `CLIENT_PUBKEY_...`
    
    Since the Debian openssh-server is relatively old, fingerprints
    are shown as md5sum, while most clients show sha256. To match
    the two, use `FingerprintHash md5` in your .ssh/config for
    the relevant Host entries.

--- setup_ssh.sh
+++ setup_ssh.sh
@@ -3,16 +3,19 @@
 # Setup client's public-key
 mkdir -p ~/.ssh
 # CLIENT_PUBKEY is set via resin.io env vars
-echo $CLIENT_PUBKEY | tee -a ~/.ssh/authorized_keys
+for var in ${!CLIENT_PUBKEY@}; do
+  echo "${!var}" | tee -a ~/.ssh/authorized_keys
+done
 
-if [ -d "/data/ssh" ]; then
-    # Restore keys from first openssh-server install
-    cp /data/ssh/ssh_host_*_key* /etc/ssh/
-else
-    # Save keys from first openssh-server install
-    mkdir /data/ssh
-    cp /etc/ssh/ssh_host_*_key* /data/ssh/
-
-fi
+SSH_HOST_KEYDIR="/data/ssh"
+mkdir -p "${SSH_HOST_KEYDIR}"
+KEYS=(dsa rsa ecdsa ed25519)
+for key in "${KEYS[@]}"; do
+  if [ ! -f "${SSH_HOST_KEYDIR}/ssh_host_${key}_key" ]; then
+     echo Generating ${key} key...
+     ssh-keygen -q -N '' -t ${key} -f "${SSH_HOST_KEYDIR}/ssh_host_${key}_key"
+  fi
+  ssh-keygen -l -f "${SSH_HOST_KEYDIR}/ssh_host_${key}_key"
+done
 
 service ssh start
